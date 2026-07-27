dc6a27b1e51cf23417351c9ca868b8a81cb8398c
Luís Gustavo <162190388+luispaivax@users.noreply.github.com>
2026-07-27 20:28:40 -0300

    Initial commit

---
+++ PraticandoCSharp_POO/Produto.cs
@@ -0,0 +1,7 @@
+﻿class Produto
+{
+    public string nome;
+    public string descricao;
+    public decimal preco;
+    public int estoque;
+}
---
+++ PraticandoCSharp_POO/Program.cs
@@ -0,0 +1,12 @@
+﻿Produto item1 = new Produto();
+item1.nome = "Teclado";
+item1.descricao = "Modelo compacto e silencioso," + " perfeito para produtividade diária.";
+item1.preco = 80.00m;
+item1.estoque = 15;
+
+Console.WriteLine($@"Dados do item 1:
+Nome: {item1.nome};
+Descricao: {item1.descricao};
+Preço: R${item1.preco};
+Estoque: {item1.estoque}.
+");
