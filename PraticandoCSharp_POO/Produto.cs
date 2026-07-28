c7ee6959c096dd3f7df875adfd014f736f38a005
luispaivax <paivaluis005@gmail.com>
2026-07-27 21:23:48 -0300

    POO Ex01

--- PraticandoCSharp_POO/Produto.cs
+++ PraticandoCSharp_POO/Produto.cs
@@ -1,7 +1,29 @@
-﻿class Produto
+﻿/* Aula 01 e 02 - CLASSES, OBJETOS, ATRIBUTOS E MÉTODOS
+class Produto
 {
     public string nome;
     public string descricao;
     public decimal preco;
     public int estoque;
+
+    public bool EstaDisponivel()
+    {
+        return estoque > 0;
+    }
+
+    public void AlterarPrecoComDesconto(decimal desconto)
+    {
+        preco = preco * (1 - desconto);
+    }
 }
+*/
+
+/* Ex 01
+class Livro
+{
+    public string Titulo;
+    public string Autor;
+} 
+*/
+
+
--- PraticandoCSharp_POO/Program.cs
+++ PraticandoCSharp_POO/Program.cs
@@ -1,4 +1,6 @@
-﻿Produto item1 = new Produto();
+﻿/* Aula 01 e 02 - CLASSES, OBJETOS, ATRIBUTOS E MÉTODOS
+
+Produto item1 = new Produto();
 item1.nome = "Teclado";
 item1.descricao = "Modelo compacto e silencioso," + " perfeito para produtividade diária.";
 item1.preco = 80.00m;
@@ -10,3 +12,32 @@ Descricao: {item1.descricao};
 Preço: R${item1.preco};
 Estoque: {item1.estoque}.
 ");
+
+if (item1.EstaDisponivel())
+{
+    Console.WriteLine("Produto está disponível!");
+}
+
+item1.AlterarPrecoComDesconto(0.2m);
+
+Console.WriteLine($@"Dados do item 1:
+Nome: {item1.nome};
+Descricao: {item1.descricao};
+Preço: R${item1.preco};
+Estoque: {item1.estoque}.
+");
+
+*/
+
+/* Ex01
+Livro livro1 = new Livro();
+livro1.Titulo = "Dom Casmurro";
+livro1.Autor = "Machado de Assis";
+
+Console.WriteLine(@$"Dados do livro 1: 
+Livro: {livro1.Titulo}
+Autor: {livro1.Autor}
+");
+*/
+
+
